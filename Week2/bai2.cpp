f1f4fe63e7f92df422fa2970be1ceeaf8af4cd23
Chi Khanh <83741889+khanhchi01005@users.noreply.github.com>
2024-03-05 22:06:29 +0700

    Create bai2.cpp

---
+++ Week2/bai2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >>n;
+    for(int i=n-1;i>=0;i--){
+        for(int space =0;space <n-i-1;space++){
+            cout <<" ";
+        }
+        for(int j=0;j<=i;j++){
+            cout <<"*";
+        }
+        cout <<endl;
+    }
+
+}
